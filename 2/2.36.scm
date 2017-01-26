9fc5391205875fa518ad9f35b85eb2d1d32f7c1d
Miguel Coba <miguel.coba@gmail.com>
2017-01-25 23:33:20 -0600

    Exercise 2.36.scm

---
+++ 2/2.36.scm
@@ -0,0 +1,10 @@
+;; extracts the car of each list mapping seqs with car
+;; recurses passing the cdr of each list mapping seqs with cdr
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+	    (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+;Value: (22 26 30)
