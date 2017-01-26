972da56088eba06ae832a16814936e28b2fea1c4
Miguel Coba <miguel.coba@gmail.com>
2017-01-26 11:59:54 -0600

    Exercise 2.40

--- 2/2.scm
+++ 2/2.scm
@@ -294,3 +294,41 @@
 	      0
 	      (map salary
 		   (filter programmer? records))))
+
+(accumulate append
+	    nil
+	    (map (lambda (i)
+		   (map (lambda (j) (list i j))
+			(enumerate-interval 1 (- i 1))))
+		 (enumerate-interval 1 n)))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+	       (flatmap
+		(lambda (i)
+		  (map (lambda (j) (list i j))
+		       (enumerate-interval 1 (- i 1))))
+		(enumerate-interval 1 n)))))
+
+(define (permutations s)
+  (if (null? s)      ; empty set?
+      (list nil)     ; sequence containing empty set
+      (flatmap (lambda (x)
+		 (map (lambda (p) (cons x p))
+		      (permutations (remove x s))))
+	       s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item))) sequence))
+
