b1939df40bf59c9273efe4fbc5e6ea795a82cc81
Miguel Coba <miguel.coba@gmail.com>
2017-01-25 22:39:11 -0600

    Exercise 2.33

--- 2/2.scm
+++ 2/2.scm
@@ -218,3 +218,79 @@
 	     (scale-tree sub-tree factor)
 	     (* sub-tree factor)))
        tree))
+
+; 2.2.3 Sequences as Conventional Interfaces
+
+(define nil '())
+
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+	((not (pair? tree))
+	 (if (odd? tree) (square tree) 0))
+	(else (+ (sum-odd-squares (car tree))
+		 (sum-odd-squares (cdr tree))))))
+
+(define (eve-fibs n)
+  (define (next k)
+    (if (> k n)
+	nil
+	(let ((f (fib k)))
+	  (if (even? f)
+	      (cons f (next (+ k 1)))
+	      (next (+ k 1))))))
+  (next 0))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+	((predicate (car sequence))
+	 (cons (car sequence) (filter predicate (cdr sequence))))
+	(else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence) (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+	((not (pair? tree)) (list tree))
+	(else (append (enumerate-tree (car tree))
+		      (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares tree)
+  (accumulate +
+	      0
+	      (map square
+		   (filter odd?
+			   (enumerate-tree tree)))))
+
+(define (even-fibs n)
+  (accumulate cons
+	       nil
+	       (filter even?
+		       (map fib
+			    (enumerate-interval 0 n)))))
+
+(define (list-fib-squares n)
+  (accumulate cons
+	       nil
+	       (map square
+		       (map fib
+			    (enumerate-interval 0 n)))))
+
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate *
+	      1
+	      (map square
+		   (filter odd? sequence))))
+
+(define (salary-of-highest-paid-programmer records)
+  (accumulate max
+	      0
+	      (map salary
+		   (filter programmer? records))))
