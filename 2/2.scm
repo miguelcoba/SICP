ac694aebaba78b77be45a6ee3bb6850c1d9d7f21
Miguel Coba <miguel.coba@gmail.com>
2017-01-28 00:10:29 -0600

    Exercise 2.59

--- 2/2.scm
+++ 2/2.scm
@@ -539,4 +539,23 @@
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
-	 
+
+; 2.3.3 Example: Representing Sets
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((equal? x (car set)) true)
+	(else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+	((element-of-set? (car set1) set2)
+	 (cons (car set1)
+	       (intersection-set (cdr set1) set2)))
+	(else (intersection-set (cdr set1) set2))))
+
