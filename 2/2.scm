0530f7fb19ea6f6b2b9b07a9e27504ad96646260
Miguel Coba <miguel.coba@gmail.com>
2017-01-29 21:54:02 -0600

    Exercise 2.75

--- 2/2.scm
+++ 2/2.scm
@@ -916,3 +916,18 @@
   ((get 'make-from-real-imag 'rectangular) x y))
 (define (make-from-mag-ang r a)
   ((get 'make-from-mag-ang 'polar) r a))
+
+;; Message passing
+
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else
+           (error "Unknown op -- MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
+(define (apply-generic op arg) (arg op))
