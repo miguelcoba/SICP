de81f8f3078d634d56b215203ad69c9b2cbfa6e0
Miguel Coba <miguel.coba@gmail.com>
2017-01-31 00:20:20 -0600

    Exercise 2.81

--- 2/2.scm
+++ 2/2.scm
@@ -1041,3 +1041,48 @@
   ((get 'make-from-real-imag 'complex) x y))
 (define (make-complex-from-mag-ang r a)
   ((get 'make-from-mag-ang 'complex) r a))
+
+;; Combining Data of Different Types
+
+(define (add-complex-to-schemenum z x)
+  (make-from-real-imag (+ (real-part z) x)
+		       (imag-part z)))
+
+(put 'add '(complex scheme-number)
+     (lambda (z x) (tag (add-complex-to-schemenum z x))))
+
+;; Coercion
+
+(define (scheme-number->complex n)
+  (make-complex-from-real-imag (contents n) 0))
+
+(put-coercion 'scheme-number 'cmoplex scheme-number->complex)
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+	  (apply proc (map contents args))
+	  (if (= (length args) 2)
+	      (let ((type1 (car type-tags))
+		    (type2 (cadr type-tags))
+		    (a1 (car args))
+		    (a2 (cadr args)))
+		(let ((t1->t2 (get-coercion type1 type2))
+		      (t2->t1 (get-coercion type2 type1)))
+		  (cond (t1->t2 (apply-generic op (t1->t2 a1) a2))
+			(t2->t1 (apply-generic op a1 (t2->t1 a2)))
+			(else
+			 (error "No method for these types"
+				(list op type-tags))))))
+	      (error "No method for these types"
+		     (list op type-tags)))))))
+
+;; Hierarchies of types
+
+;; Inadequacies of hierarchies
+
+
+
+		      
+			
