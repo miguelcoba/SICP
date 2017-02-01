1b95435119f4d679dd9a3d74927580c16885aa59
Miguel Coba <miguel.coba@gmail.com>
2017-02-01 14:09:19 -0600

    Exercise 3.7

--- 3/3.scm
+++ 3/3.scm
@@ -105,3 +105,35 @@
                      trials-passed
                      x2))))))
   (iter trials 0 initial-x))
+
+;; The Costs of Introducing Assignment
+
+(define (make-simplified-withdraw balance)
+  (lambda (amount)
+    (set! balance (- balance amount))
+    balance))
+(define W (make-simplified-withdraw 25))
+
+(W 20)
+
+(W 10)
+
+(define (make-decrementer balance)
+  (lambda (amount)
+    (- balance amount)))
+
+(define D (make-decrementer 25))
+
+(D 20)
+
+(D 10)
+
+;; Sameness and change
+
+(define D1 (make-decrementer 25))
+(define D2 (make-decrementer 25))
+
+(define W1 (make-simplified-withdraw 25))
+(define W2 (make-simplified-withdraw 25))
+
+;; Pitfalls of imperative programming
