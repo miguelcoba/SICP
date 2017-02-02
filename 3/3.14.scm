7b17c39f7c78dbc5602c2e69803ee05ff3f148e1
Miguel Coba <miguel.coba@gmail.com>
2017-02-02 17:35:19 -0600

    Exercise 3.14

---
+++ 3/3.14.scm
@@ -0,0 +1,24 @@
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+;; gets each head of the list and sets it as the cdr of a new list on each iteration over the
+;; elements of the original list
+
+(define v (list 'a 'b 'c 'd))
+
+;; v -> [ | ] -> [ | ] -> [ | ] -> [ |/]
+;;       |        |        |        |
+;;      [a]      [b]      [c]      [d]
+
+(define w (mystery v))
+
+;; w -> [ | ] -> [ | ] -> [ | ] -> [ |/]
+;;       |        |        |        |
+;;      [d]      [c]      [b]      [a]
+
