76963f77883b386dc18181432313f31ce3e94956
Miguel Coba <miguel.coba@gmail.com>
2017-02-02 14:08:38 -0600

    Section 3.2.4

--- 3/3.scm
+++ 3/3.scm
@@ -159,3 +159,16 @@
         (begin (set! balance (- balance amount))
                balance)
         "Insufficient funds")))
+
+;; 3.2.4 Internal Definitions
+
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
