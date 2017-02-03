eb385a6bb6612f60483d40b74599865ce5739b6a
Miguel Coba <miguel.coba@gmail.com>
2017-02-02 18:41:34 -0600

    Exercise 3.15

--- 3/3.scm
+++ 3/3.scm
@@ -185,3 +185,16 @@
 
 (set-cdr! x y)
 
+;; Sharing and identity
+
+(define x (list 'a 'b))
+(define z1 (cons x x))
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+(define (set-to-wow! x)
+  (set-car! (car x) 'wow)
+  x)
+(set-to-wow! z1)
+(set-to-wow! z2)
+
+
+
