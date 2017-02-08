5da7ef19a081026e256a6209c554a5b81dce99f3
Miguel Coba <miguel.coba@gmail.com>
2017-02-08 00:16:51 -0600

    Exercise 3.33 using multiplier, adder and constant

--- 3/3.33.scm
+++ 3/3.33.scm
@@ -1,30 +1,10 @@
+;; c = (a + b) / 2
+;; 2c = a + b
 (define (averager a b c)
-  (define (process-new-value)
-    (cond ((and (has-value? a) (has-value? b))
-	   ;; c = (a + b)/2
-	   (set-value! c
-		       (/ (+ (get-value a) (get-value b)) 2)
-		       me))
-	  ((and (has-value? a) (has-value? c))
-	   ;; c = (a + b)/2, 2c = a + b, b = 2c - a
-	   (set-value! b
-		       (- (* 2 (get-value c)) (get-value a))
-		       me))
-	  ((and (has-value? b) (has-value? c))
-	   ;; c = (a + b)/2, 2c = a + b, a = 2c - b
-	   (set-value! a
-		       (- (* 2 (get-value c)) (get-value b))
-		       me))))
-  (define (process-forget-value)
-    (forget-value! c me)
-    (forget-value! a me)
-    (forget-value! b me)
-    (process-new-value))
-  (define (me request)
-    (cond ((eq? request 'I-have-a-value) (process-new-value))
-	  ((eq? request 'I-lost-my-value) (process-forget-value))
-	  (else (error "Unknown request -- AVERAGER" request))))
-  (connect a me)
-  (connect b me)
-  (connect c me)
-  me)
+  (let ((sum (make-connector))
+	(const (make-connector)))
+    (adder a b sum)
+    (constant 2 const)
+    (multipier const c sum)
+    'ok))
+
