2196153926eaea47bf473591534f38634f58ff9f
Miguel Coba <miguel.coba@gmail.com>
2017-02-12 12:50:58 -0600

    Exercise 3.51

---
+++ 3/3.51.scm
@@ -0,0 +1,20 @@
+(define (show x)
+  (display-line x)
+  x)
+
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+0
+;Value: x
+
+(stream-ref x 5)
+1
+2
+3
+4
+5
+;Value: 5
+
+(stream-ref x 7)
+6
+7
+;Value: 7
