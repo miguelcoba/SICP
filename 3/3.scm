c8903ed8451989e4a3f63a17e62f2a7e87ac224d
Miguel Coba <miguel.coba@gmail.com>
2017-02-11 18:44:31 -0600

    Exercise 3.39

--- 3/3.scm
+++ 3/3.scm
@@ -740,3 +740,40 @@
 
 ;; Correct behavior of concurrent programs
 
+;; 3.4.2 Mechanisms for Controlling Concurrency
+
+;; Serializing access to shared state
+
+;; Serializers in Scheme
+
+(define x 10)
+
+(parallel-execute (lambda () (set! x (* x x)))
+		  (lambda () (set! x (+ x 1))))
+
+(define x 10)
+
+(define s (make-serializer))
+
+(parallel-execute (s (lambda () (set! x (* x x))))
+		  (s (lambda () (set! x (+ x 1)))))
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance) balance)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+
+
