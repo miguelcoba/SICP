356e29692141275d2e4a158b959cd821cb07c6df
Miguel Coba <miguel.coba@gmail.com>
2017-02-13 21:29:42 -0600

    Exercise 3.63

--- 3/3.scm
+++ 3/3.scm
@@ -946,3 +946,50 @@
           ((divisible? n (stream-car ps)) false)
           (else (iter (stream-cdr ps)))))
   (iter primes))
+
+;; 3.5.3 Exploiting the Stream Paradigm
+
+;; Formulating iterations as stream processes
+
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 1.0
+		 (stream-map (lambda (guess)
+			       (sqrt-improve guess x))
+			     guesses)))
+  guesses)
+
+(display-stream (sqrt-stream 2))
+
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+	       (stream-map - (pi-summands (+ n 2)))))
+
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+(display-stream pi-stream)
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0)) ; Sn-1
+	(s1 (stream-ref s 1)) ; Sn
+	(s2 (stream-ref s 2))); Sn+1
+    (cons-stream (- s2 (/ (square (- s2 s1))
+			  (+ s0 (* -2 s1) s2)))
+		 (euler-transform (stream-cdr s)))))
+
+(display-stream (euler-transform pi-stream))
+
+(define (make-tableau transform s)
+  (cons-stream s
+	       (make-tableau transform
+			     (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car
+	      (make-tableau transform s)))
+
+(display-stream (accelerated-sequence euler-transform pi-stream))
