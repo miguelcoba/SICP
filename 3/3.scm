767d420e01619f2443434a3ad6403be4aeb3c8b5
Miguel Coba <miguel.coba@gmail.com>
2017-02-14 00:46:19 -0600

    Section 3.4

--- 3/3.scm
+++ 3/3.scm
@@ -1033,3 +1033,33 @@
 		 (add-streams (scale-stream integrand dt)
 			      int)))
   int)
+
+;; 3.5.4 Streams and Delayed Evaluation
+
+(define int
+  (cons-stream initial-value
+               (add-streams (scale-stream integrand dt)
+                            int)))
+
+(define (solve f y0 dt)
+  (define y (integral dy y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+(define (integral delayed-integrand initial-value dt)
+  (define int
+    (cons-stream initial-value
+                 (let ((integrand (force delayed-integrand)))
+                   (add-streams (scale-stream integrand dt)
+                                int))))
+  int)
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+(stream-ref (solve (lambda (y) y) 1 0.001) 1000)
+
+;; Normal-order evaluation
+
