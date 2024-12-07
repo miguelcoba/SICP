bbb08e41191e0e4b268f2f84bae05fcce9faa67c
Miguel Coba <miguel.coba@gmail.com>
2024-12-07 21:19:23 +0100

    Section 1.2

--- 1/1.scm
+++ 1/1.scm
@@ -1,4 +1,17 @@
 ;; 1.1 The Elements of Programming
+
+;; 1.1.1 Expressions
+
+;; 1.1.2 Naming and the Environment
+
+;; 1.1.3 Evaluating Combinations
+
+;; 1.1.4 Compound Procedures
+
+;; 1.1.5 The Substitution Model for Procedure Application
+
+;; 1.1.6 Conditional Expressions and Predicates
+
 ;; 1.1.7 Example: Square Roots by Newton's Method
 
 (define (square x)
@@ -7,11 +20,11 @@
 (define (sum-of-squares x y)
   (+ (square x) (square y)))
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
 (define (average x y)
   (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
 
 (define (improve guess x)
   (average guess (/ x guess)))
@@ -23,3 +36,35 @@
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
+
+;; 1.1.8 Procedures as Black-Box Abstractions
+
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+	guess
+	(sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+
+;; 1.2 Procedures and the Processes They Generate
+
+;; 1.2.1 Linear Recursion and Iteration
+
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+(define (factorial n)
+  (fact-iter 1 1 n))
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+		 (+ counter 1)
+		 max-count)))
+
