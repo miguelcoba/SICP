4832646b53f572632a66fddd39245a6a88a2342f
Miguel Coba <miguel.coba@gmail.com>
2024-12-09 21:52:27 +0000

    Exercise 1.17

---
+++ 1/1.17.scm
@@ -0,0 +1,15 @@
+(define (* a b)
+  (if (= b 0)
+      0
+      (+ a (* a (- b 1)))))
+
+(define (double x)
+  (+ x x))
+
+(define (halve x)
+  (/ x 2))
+
+(define (* a b)
+  (cond ((= b 0) 0)
+	((even? b) (double (* a (halve b))))
+	(else (+ a (* a (- b 1))))))
